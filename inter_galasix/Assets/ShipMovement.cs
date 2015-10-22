ce9ab827c718de7d3e5e41197cd4c2a5eb1073c0
Nikola Lukic <nikolalkc@hotmail.com>
2015-10-22 23:28:23 +0200

    prvi test demo. kretanje

---
+++ inter_galasix/Assets/ShipMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+	Rigidbody2D rig;
+	public float forceFactor, torqueFactor;
+	public Transform direction;
+
+
+	void Start () {
+		rig = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () {
+		if (Input.GetAxis("Vertical") > 0) {
+
+			rig.AddForce(transform.up * forceFactor * Time.deltaTime * Input.GetAxis("Vertical"));
+		
+		 }
+
+		if (Input.GetAxis("Horizontal") != 0) {
+			rig.AddTorque(Input.GetAxis("Horizontal") * torqueFactor * -1 * Time.deltaTime);
+		}
+
+	}
+}
