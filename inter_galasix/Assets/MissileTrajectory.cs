7c343d8bbbdd20b1241a906ba57a786a997ca70d
Nikola Lukic <nikolalkc@hotmail.com>
2015-10-27 12:06:11 +0100

    raketa uradjena da prati playera

---
+++ inter_galasix/Assets/MissileTrajectory.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MissileTrajectory : MonoBehaviour {
+	List<GameObject> player = new List<GameObject>();
+	GameObject target_player;
+	public float speedFactor;
+	float randomSpeedVariation = 2;
+	Rigidbody2D rig;
+
+	void Start () {
+		rig = GetComponent<Rigidbody2D>();
+
+		//brzina rakete
+		randomSpeedVariation = Random.Range(-randomSpeedVariation, randomSpeedVariation);
+		speedFactor += randomSpeedVariation;
+
+		//izbor random igraca kojeg ce raketa da prati
+		foreach (GameObject element in GameObject.FindGameObjectsWithTag("Player")) {
+			player.Add(element);
+		}
+
+
+		if (player.Count > 0) {
+			int random_index = Random.Range(0, player.Count);
+			target_player = player[random_index];
+		}
+		
+	}
+
+	void FixedUpdate () {
+		//prati igraca
+		if (target_player) {
+			Vector3 dir = target_player.transform.position - transform.position;
+			float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			rig.velocity = transform.right * speedFactor;
+
+			
+		}
+	
+	}
+}
