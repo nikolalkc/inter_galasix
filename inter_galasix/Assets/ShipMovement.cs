a5fc8ddd6d781a8e907021469f03477f81a3b0dc
Nikola Lukic <nikolalkc@hotmail.com>
2015-10-28 18:21:05 +0100

    slider za turbo

--- inter_galasix/Assets/ShipMovement.cs
+++ inter_galasix/Assets/ShipMovement.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets._2D;
+using UnityEngine.UI;
 
-public class ShipMovement : MonoBehaviour {
+public class ShipMovement : MonoBehaviour
+{
 	Rigidbody2D rig;
 	public float forceFactor, torqueFactor, forceBoost;
 	public Transform direction;
@@ -15,53 +17,56 @@ public class ShipMovement : MonoBehaviour {
 	Camera pov;
 	Camera main;
 	bool fps = false;
+	public Slider TurboStats;
 	//zoomout preko pomeranja kamere po z osi
-		//Vector3 original_camera_position = new Vector3();
-		//Vector3 zoomout_position = new Vector3();
-		//Camera2DFollow camera2DFollow;
-		//bool new_zoomout_position_set = false;
+	//Vector3 original_camera_position = new Vector3();
+	//Vector3 zoomout_position = new Vector3();
+	//Camera2DFollow camera2DFollow;
+	//bool new_zoomout_position_set = false;
 
-	void Start () {
+	void Start ()
+	{
+		TurboStats = GameObject.Find ("TurboStat").GetComponent<Slider> ();
 		main = Camera.main;
 		original_FOV = Camera.main.fieldOfView;
-		rig = GetComponent<Rigidbody2D>();
-		pov = GetComponentInChildren<Camera>();
+		rig = GetComponent<Rigidbody2D> ();
+		pov = GetComponentInChildren<Camera> ();
 		//zoomout preko pomeranja kamere po z osi
-			//original_camera_position = Camera.main.transform.position;
-			//zoomout_position = original_camera_position + new Vector3(0, 0, -30);
-			//camera2DFollow = Camera.main.GetComponent<Camera2DFollow>();
+		//original_camera_position = Camera.main.transform.position;
+		//zoomout_position = original_camera_position + new Vector3(0, 0, -30);
+		//camera2DFollow = Camera.main.GetComponent<Camera2DFollow>();
 		
 	}
-	
 
-	void Update () {
+	void Update ()
+	{
 		//gas
-		if (Input.GetAxis("Vertical") > 0) {
+		if (Input.GetAxis ("Vertical") > 0) {
 
-			rig.AddForce(transform.up * forceFactor * Time.deltaTime * Input.GetAxis("Vertical"));
+			rig.AddForce (transform.up * forceFactor * Time.deltaTime * Input.GetAxis ("Vertical"));
 		
-		 }
+		}
 		//rotiranje
-		if (Input.GetAxis("Horizontal") != 0) {
-			rig.AddTorque(Input.GetAxis("Horizontal") * torqueFactor * -1 * Time.deltaTime);
+		if (Input.GetAxis ("Horizontal") != 0) {
+			rig.AddTorque (Input.GetAxis ("Horizontal") * torqueFactor * -1 * Time.deltaTime);
 		}
 
 		//pucanje
-		if (Input.GetButtonDown("Fire")) {
-			PhotonNetwork.Instantiate("bullet", bulletSpawnPoint.position, transform.rotation,0);
+		if (Input.GetButtonDown ("Fire")) {
+			PhotonNetwork.Instantiate ("bullet", bulletSpawnPoint.position, transform.rotation, 0);
 			
 		}
 
 		//ukljuci pov
-		if (Input.GetKeyDown(KeyCode.C)) {
-			pov.enabled=true;
+		if (Input.GetKeyDown (KeyCode.C)) {
+			pov.enabled = true;
 			main.enabled = false;
 			fps = true;
 		}
 		//iskljuci pov
-		if (Input.GetKeyUp(KeyCode.C)) {
+		if (Input.GetKeyUp (KeyCode.C)) {
 			pov.enabled = false;
-			main.enabled=true;
+			main.enabled = true;
 			fps = false;
 		}
 
@@ -110,59 +115,65 @@ public class ShipMovement : MonoBehaviour {
 
 
 
-		//nitro
-		//rig.AddForce(transform.up * forceBoost);
 
-		bool doubleTapUp = false;
 
-		#region doubleTapRight
 
-		if (Input.GetButton("Boost")) {
-			if (Time.time < _doubleTapTimeUp + .3f) {
-				doubleTapUp = true;
+		// puni nitro
+		if (Input.GetButton ("Boost")) {
+			CancelInvoke("RechargeNitro");
+			TurboStats.value -= 1;
+			if (TurboStats.value > 0) {
+				rig.AddForce (transform.up * forceBoost);
 			}
-			_doubleTapTimeUp = Time.time;
+		} 
+		// trosi nitro
+		else {
+			if (TurboStats.value < 100) {
+				Invoke ("RechargeNitro",3f);
+			}
 		}
 
-		#endregion
-
-		if (doubleTapUp) {
-			rig.AddForce(transform.up * forceBoost);
-		}
 
 
 		
 		//zoomout preko pomeranja kamere po z osi
-			//if (Input.GetKey(KeyCode.LeftShift)) {
+		//if (Input.GetKey(KeyCode.LeftShift)) {
 
-			//	if (!new_zoomout_position_set) {
-			//		zoomout_position = Camera.main.transform.position + new Vector3(0, 0, -30);
-			//		new_zoomout_position_set = true;
-			//	}
+		//	if (!new_zoomout_position_set) {
+		//		zoomout_position = Camera.main.transform.position + new Vector3(0, 0, -30);
+		//		new_zoomout_position_set = true;
+		//	}
 
-			//	camera2DFollow.enabled = false;
-			//	Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position,
-			//												  zoomout_position,
-			//												  5 * Time.deltaTime);
-			//}
-			//else {
-			//	camera2DFollow.enabled = true;
-			//	new_zoomout_position_set = false;
-			//}
+		//	camera2DFollow.enabled = false;
+		//	Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position,
+		//												  zoomout_position,
+		//												  5 * Time.deltaTime);
+		//}
+		//else {
+		//	camera2DFollow.enabled = true;
+		//	new_zoomout_position_set = false;
+		//}
 
 
 		//zoomout preko FOV 
 		if (!fps) {
 			Camera camera = Camera.main;
-			if (Input.GetButton("Zoomout")) {
-				camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, zoomout_FOV, 5 * Time.deltaTime);
-			}
-			else {
-				camera.fieldOfView = Mathf.Lerp(camera.fieldOfView, original_FOV, 5 * Time.deltaTime);
+			if (Input.GetButton ("Zoomout")) {
+				camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoomout_FOV, 5 * Time.deltaTime);
+			} else {
+				camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, original_FOV, 5 * Time.deltaTime);
 			}
 		}
 
 
+
+
+	}
+
+	void RechargeNitro(){
+		if (TurboStats.value<100) {
+			TurboStats.value += 0.1f;
+		}
 
 
 	}
